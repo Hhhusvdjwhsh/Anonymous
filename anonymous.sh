91338d3b9afbd2ba8079ae3bbdfe693d75d99592
Hhhusvdjwhsh <123642331+Hhhusvdjwhsh@users.noreply.github.com>
2024-05-04 00:16:08 +0700

    Update anonymous.sh

--- anonymous.sh
+++ anonymous.sh
@@ -30,6 +30,8 @@ apt install npm -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
 nvm install node
+source ~/.bashrc
+
 
 # Upgrade npm
 npm i npm@latest -g
